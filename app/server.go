0412c9de24780df9b2ced6c7a47bd663e82a8a59
SaiSawant1 <saisawant2003@gmail.com>
2024-11-30 15:25:06 +0530

    [any message]

--- app/server.go
+++ app/server.go
@@ -20,7 +20,7 @@ func main() {
 		os.Exit(1)
 	}
 	conn, err := l.Accept()
-	conn.Write([]byte{0, 0, 0, 0, 0, 0, 7})
+	conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
