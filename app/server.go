d5e9872929db0328551a2e9fd1708f27cdddd121
SaiSawant1 <saisawant2003@gmail.com>
2024-11-30 19:25:45 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"time"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
+var TAG_BUFFER = []byte{0x00}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
+	startServer()
+
+}
+
+func startServer() {
+
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
-		fmt.Println("Failed to bind to port 9092", err.Error())
+		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	defer l.Close()
 
 	conn, err := l.Accept()
 	if err != nil {
@@ -29,38 +40,66 @@ func main() {
 	}
 	defer conn.Close()
 
-	buff := make([]byte, 1024)
-	_, err = conn.Read(buff)
+	for {
+		req, err := NewRequestFromConn(conn)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(req)
+
+		res := bytes.NewBuffer([]byte{})
+
+		binary.Write(res, binary.BigEndian, uint32(req.CorrelationID))
+
+		var errorCode = uint16(0)
+
+		if int16(req.ApiVersion) < 0 || int16(req.ApiVersion) > 4 {
+			errorCode = uint16(35)
+		}
+
+		binary.Write(res, binary.BigEndian, uint16(errorCode))
+		binary.Write(res, binary.BigEndian, byte(2))
+		binary.Write(res, binary.BigEndian, uint16(2))
+		binary.Write(res, binary.BigEndian, uint16(18))
+		binary.Write(res, binary.BigEndian, uint16(3))
+		binary.Write(res, binary.BigEndian, uint16(4))
+
+		res.Write(TAG_BUFFER)
+
+		binary.Write(res, binary.BigEndian, res.Len())
+		io.Copy(conn, res)
+
+	}
+
+}
+
+type Request struct {
+	ApiKey        uint16
+	ApiVersion    uint16
+	CorrelationID uint32
+	ClientId      string
+	TaggedFields  string
+}
+
+func NewRequestFromConn(conn net.Conn) (Request, error) {
+	var size uint32
+
+	err := binary.Read(conn, binary.BigEndian, &size)
 	if err != nil {
-		fmt.Println("Failed to read from connection", err.Error())
-		os.Exit(1)
+		return Request{}, err
 	}
 
-	length := []byte{0, 0, 0, 19}
+	var req = Request{}
 
-	api_version := buff[6:8]
+	binary.Read(conn, binary.BigEndian, &req.ApiKey)
+	binary.Read(conn, binary.BigEndian, &req.ApiVersion)
+	binary.Read(conn, binary.BigEndian, &req.CorrelationID)
 
-	correlation_id := buff[8:12]
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 
-	var version_error []byte
+	conn.Read(make([]byte, 1024))
 
-	switch binary.BigEndian.Uint16(api_version) {
-
-	case 0, 1, 2, 3, 4:
-		version_error = []byte{0, 0}
-	default:
-		version_error = []byte{0, 35}
-
-	}
-
-	res := append(length, correlation_id...)
-	res = append(res, version_error...)
-	_, err = conn.Write(res)
-	if err != nil {
-		fmt.Println("Failed to read from connection", err.Error())
-		os.Exit(1)
-	}
-
-	ret_key := []byte{2, 0, 18, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0}
-	conn.Write(ret_key)
+	return req, nil
 }
